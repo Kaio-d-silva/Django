ae7c0147bcf040010c32ca73d0cb633f174680e4
Kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2024-12-05 21:32:00 -0400

    alterações no banco

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,45 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, HttpRequest
+from django.views.decorators.http import require_POST
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+@require_POST
+def index(request: HttpRequest):
+    try:
+        bodyreq = request.body
+        return HttpResponse(bodyreq)
+    except:
+        ...
+        
+@require_POST
+def cadastro_paciente(request: HttpRequest):
+    try:
+        bodyreq = request.body
+        return HttpResponse(bodyreq)
+    except:
+        ...
+
+@require_POST        
+def cadastro_endereco(request: HttpRequest):
+    try:
+        bodyreq = request.body
+        return HttpResponse(bodyreq)
+    except:
+        ...
+
+@require_POST        
+def cadastro_clinica(request: HttpRequest):
+    try:
+        bodyreq = request.body
+        return HttpResponse(bodyreq)
+    except:
+        ...
+
+@require_POST        
+def cadastro_profissional(request: HttpRequest):
+    try:
+        bodyreq = request.body
+        return HttpResponse(bodyreq)
+    except:
+        ...
