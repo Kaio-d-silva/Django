9811d3c59c5d0be1623ccaf5e4aa1123646e9b56
Kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2024-12-11 20:15:26 -0400

    11-12

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,36 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse, HttpRequest
+from django.views.decorators.http import require_POST
+from models import Choice, Question
 
+
+@require_POST
 def index(request: HttpRequest):
     try:
         bodyreq = request.body
+        return HttpResponse(bodyreq)
+    except:
+        ...
+        
+@require_POST
+def question(request: HttpRequest):
+    try:
+        bodyreq = request.body
+        Question.objects.create(question_class='', pub_date='')
+        return HttpResponse(bodyreq)
+    except:
+        ...
+        
+  
+  
+  
+        
+@require_POST
+def choice(request: HttpRequest):
+    try:
+        bodyreq = request.body
+        Choice.objects.create(question='',choice_texte='',votes='')
         return HttpResponse(bodyreq)
     except:
         ...
--- polls/models.py
+++ polls/models.py
@@ -13,35 +13,35 @@ from django.db import models
 #     votes = models.IntegerField(default=0)
  
    
-class endereco(models.Model):
-    id = models.AutoField(primary_key=True)
-    rua = models.CharField(max_length=30)
-    bairro = models.CharField(max_length=20)
-    cidade = models.CharField(max_length=20)
-    numero = models.IntegerField()
+# class endereco(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     rua = models.CharField(max_length=30)
+#     bairro = models.CharField(max_length=20)
+#     cidade = models.CharField(max_length=20)
+#     numero = models.IntegerField()
     
-class paciente(models.Model):
-    id = models.AutoField(primary_key=True)
-    # endereco_id = models.ForeignKey(endereco)
-    nome = models.CharField(max_length=40)
-    data_nascimento = models.DateTimeField()
-    cpf = models.IntegerField()
-    telefone = models.IntegerField()
+# class paciente(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     # endereco_id = models.ForeignKey(endereco)
+#     nome = models.CharField(max_length=40)
+#     data_nascimento = models.DateTimeField()
+#     cpf = models.IntegerField()
+#     telefone = models.IntegerField()
     
 
-class clinica(models.Model):
-    id = models.AutoField(primary_key=True)
-    # endereco_id = models.ForeignKey(endereco)
-    nome = models.CharField(max_length=30)
-    cnpj = models.IntegerField()
+# class clinica(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     # endereco_id = models.ForeignKey(endereco)
+#     nome = models.CharField(max_length=30)
+#     cnpj = models.IntegerField()
     
     
-class profissional_clinica(models.Model):
-    id = models.AutoField(primary_key=True)
-    # tipo = 0
-    nome = models.CharField(max_length=30)
-    registro_profissional = models.IntegerField()
-    tipo_registro = models.CharField(max_length=30)
+# class profissional_clinica(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     # tipo = 0
+#     nome = models.CharField(max_length=30)
+#     registro_profissional = models.IntegerField()
+#     tipo_registro = models.CharField(max_length=30)
     
 
     
